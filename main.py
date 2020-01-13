440f38711fdd2346d871f98a7ce78f47d093583f
311910296 <59810906+311910296@users.noreply.github.com>
2020-01-12 20:58:27 -0800

    Update main.py

--- main.py
+++ main.py
@@ -7,3 +7,39 @@ if __name__ == "__main__":
     app = MainApp()
     app.run()
 
+    
+    
+from core.search_helper import SearchHelper
+from core.baseapp import BaseApp
+from view.view_book import ViewBook
+from view.input_book import InputBook
+
+
+class MainApp(BaseApp):
+    def init(self):
+        self.books = []
+
+
+if name == "__main__":
+    app = MainApp()
+    app.run()
+
+
+def list_book(self):
+    view = ViewBook(self.books)
+    view.list()
+
+
+def add_book(self):
+    add = InputBook()
+    add.input()
+
+
+def search_book():
+    cari = InputBook()
+    cari.search()
+    help = SearchHelper()
+    help.search_title()
+    liat = ViewBook(books=self)
+    liat.list()
+
